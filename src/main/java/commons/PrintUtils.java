d809898ca555fed4beacfee42655ec044b0ea541
Yonghwi Kim <yh0921k@gmail.com>
2022-09-19 13:52:30 +0900

    add: 알고리즘 풀이 결과인 배열, 리스트 등을 출력하기 위한 유틸리티 클래스 구현
    - 출력 인자가 원시 배열인 경우 PrimitiveArrayWrapper를 사용하여 Wrapper 배열로 변환
    - 모든 케이스에 대해 Iterator를 생성하여 각 인자를 출력

---
+++ src/main/java/commons/PrintUtils.java
@@ -0,0 +1,43 @@
+package commons;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class PrintUtils<T> {
+  public static void print(int[] array) {
+    Integer[] boxedArray = PrimitiveArrayWrapper.wrap(array);
+    print(boxedArray);
+  }
+
+  public static void print(long[] array) {
+    Long[] boxedArray = PrimitiveArrayWrapper.wrap(array);
+    print(boxedArray);
+  }
+
+  public static void print(double[] array) {
+    Double[] boxedArray = PrimitiveArrayWrapper.wrap(array);
+    print(boxedArray);
+  }
+
+  public static <T> void print(List<T> list) {
+    Iterator<T> iterator = list.listIterator();
+    printIterator(iterator);
+  }
+
+  public static <T> void print(T[] array) {
+    Iterator<T> iterator = Arrays.stream(array).iterator();
+    printIterator(iterator);
+  }
+
+  private static <T> void printIterator(Iterator<T> iterator) {
+    System.out.print("\nIterator : [");
+    while(iterator.hasNext()) {
+      System.out.print(iterator.next());
+      if(iterator.hasNext()) {
+        System.out.print(", ");
+      }
+    }
+    System.out.print("]\n");
+  }
+}

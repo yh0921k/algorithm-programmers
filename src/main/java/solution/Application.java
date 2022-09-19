4616e69f9584b608a87afea814803e935cdfffb7
Yonghwi Kim <yh0921k@gmail.com>
2022-09-19 13:52:56 +0900

    mod: 메인 어플리케이션 로직에 구현된 유틸리티 클래스 적용

--- src/main/java/solution/Application.java
+++ src/main/java/solution/Application.java
@@ -1,17 +1,18 @@
 package solution;
 
-import solution.level1.Problem006;
+import commons.PrintUtils;
+import commons.StopWatch;
 import solution.level1.Problem007;
 
 class Application {
   public static void main(String[] args) {
     Problem007 problem007 = new Problem007();
-    long start = System.nanoTime();
 
-    System.out.println(problem007.solution(4, 5));
+    StopWatch.start();
+    long[] solution = problem007.solution(-4, 20);
+    StopWatch.stop();
 
-    long finish = System.nanoTime();
-    long timeElapsed = finish - start;
-    System.out.println("Time : " + (double)timeElapsed / 1_000_000);
+    PrintUtils.print(solution);
+    StopWatch.toMilliSeconds();
   }
 }

47df7fdb5466c61bae3a039cf42344b08dcbb17f
Yonghwi Kim <yh0921k@gmail.com>
2022-09-26 21:24:06 +0900

    add: Lv 1. 내적
    - 두 배열은 길이가 같으므로, 한쪽의 길이를 이용해 반복문 순회
    - 내부에서 두 배열의 원소 곱을 합하여 반환

--- src/main/java/solution/Application.java
+++ src/main/java/solution/Application.java
@@ -2,14 +2,14 @@ package solution;
 
 import commons.PrintUtils;
 import commons.StopWatch;
-import solution.level1.Problem022;
+import solution.level1.Problem023;
 
 class Application {
   public static void main(String[] args) {
-    Problem022 problem = new Problem022();
+    Problem023 problem = new Problem023();
 
     StopWatch.start();
-    int solution = problem.solution(new int[]{1,2,3,4,6,7,8,0});
+    int solution = problem.solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2});
     StopWatch.stop();
 
     PrintUtils.print(solution);
---
+++ src/main/java/solution/level1/Problem023.java
@@ -0,0 +1,16 @@
+package solution.level1;
+
+/*
+ * 내적
+ */
+public class Problem023 {
+  public int solution(int[] a, int[] b) {
+    int answer = 0;
+
+    for (int i = 0; i < a.length; i++) {
+      answer += a[i] * b[i];
+    }
+
+    return answer;
+  }
+}
